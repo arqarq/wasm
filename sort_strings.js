402d3dbcb9a56b1c0c3ab2ec691405f31a6505c1
arqarq <43341071+arqarq@users.noreply.github.com>
2022-01-05 00:48:48 +0100

    * ...

--- sort_strings.js
+++ sort_strings.js
@@ -1,18 +1,15 @@
 function createUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16
-        console.log(r)
-        r = r | 0
-        console.log(r)
-        var v = c === 'x' ? r : r & 0x3 | 0x8
-        return v.toString(16)
-    })
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0
+    c === 'y' && (r = r & 0x3 | 0x8)
+    return r.toString(16)
+  })
 }
 
 const table = []
 
-for (let i = 0; i < 10; i++) {
-    table.push(createUUID())
+for (let i = 0; i < 100; i++) {
+  table.push(createUUID())
 }
 console.log(table)
 console.log(table.length)
