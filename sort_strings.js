9c5a26e6c13a2cb7eea0a84dfed0b03db904dc1d
arqarq <43341071+arqarq@users.noreply.github.com>
2022-01-12 01:10:17 +0100

    * ...

--- sort_strings.js
+++ sort_strings.js
@@ -1,15 +1,38 @@
+const template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx', tmplL = template.length, table = []
+let i, j, k, t1, t2, c1, c2, tabL = 50
+
 function createUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = Math.random() * 16 | 0
-    c === 'y' && (r = r & 0x3 | 0x8)
-    return r.toString(16)
-  })
+  return template.replace(/[xy]/g, replacer)
 }
 
-const table = []
+function replacer(c) {
+  let r = Math.random() * 16 | 0
+  c === 'y' && (r = r & 0x3 | 0x8)
+  return r.toString(16)
+}
 
-for (let i = 0; i < 100; i++) {
+for (i = 0; i < tabL; i++) {
   table.push(createUUID())
 }
+tabL--
+table[tabL] = '00000000-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+table[0] = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+for (j = 0; j < tabL; j++) {
+  for (i = 0; i < tabL; i++) {
+    t1 = table[i]
+    t2 = table[i + 1]
+    for (k = 0; k < tmplL; k++) {
+      c1 = t1.charCodeAt(k)
+      c2 = t2.charCodeAt(k)
+      if (c1 === c2) {
+        continue
+      }
+      if (c1 - c2 > 0) {
+        table[i + 1] = t1
+        table[i] = t2
+      }
+      break
+    }
+  }
+}
 console.log(table)
-console.log(table.length)
