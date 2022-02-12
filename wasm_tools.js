cd41835db8bbdbbcfe20c42d0417829807b11bda
arqarq <43341071+arqarq@users.noreply.github.com>
2022-02-12 20:11:56 +0100

    * ...

--- wasm_tools.js
+++ wasm_tools.js
@@ -4,7 +4,8 @@ const wabt = require('wabt')
 async function parseWat(fileName) {
   const toBinaryResult = (await wabt()).parseWat(fileName, fs.readFileSync(fileName, 'utf-8'))
     .toBinary({})
-  return (await WebAssembly.instantiate(toBinaryResult.buffer)).instance.exports
+  const {add, square, squareSquarePlus} = (await WebAssembly.instantiate(toBinaryResult.buffer)).instance.exports
+  return {add, square, squareSquarePlus}
 }
 
 module.exports = {
